e76ac04aecc116b6ccf08e9f3f6108ff9c0bfda0
Calamity Lorenzo <lawrence_paul@hotmail.co.uk>
2017-05-08 05:41:27 +0100

    Conversion Idea

---
+++ DbTests/Client/ClentExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using BooksCatalogueDb.BookInterface;
+
+namespace DbTests.Client
+{
+    public static class ClentExtension
+    {
+        public static IBook ToClient(this IBook @this)
+        {
+            return null;
+        }
+
+        public static IEdition ToClient(this IEdition @this)
+        {
+            return null;
+        }
+
+        public static IEditionFile ToClient(this IEditionFile @this)
+        {
+            return null;
+        }
+    }
+}
